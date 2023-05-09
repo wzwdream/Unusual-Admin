b4a69137598c152bb2c240d16469199fb6130b92
wangzewei <wangzewei.cq@chinatelcom.cn>
2023-05-09 15:45:51 +0800

    更新依赖

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,18 +3,12 @@
         <div class="loginForm">
             <div class="loginForm_title"><span>LOGIN</span></div>
             <n-space vertical>
-                <n-form
-                        ref="formRef"
-                        inline
-                        :label-width="150"
-                        :model="formValue"
-
-                >
+                <n-form ref="formRef" inline :label-width="150" :model="formValue">
                     <n-form-item label="" path="user.name" style="width: 80%">
-                        <n-input v-model:value="formValue.userName" placeholder="Input userName"/>
+                        <n-input v-model:value="formValue.userName" placeholder="Input userName" />
                     </n-form-item>
                     <n-form-item label="" path="user.age" style="width: 80%">
-                        <n-input v-model:value="formValue.userPassword" placeholder="Input userPassword"/>
+                        <n-input v-model:value="formValue.userPassword" placeholder="Input userPassword" />
                     </n-form-item>
                 </n-form>
                 <n-button class="loginButton" type="primary" @click="passwordEncrypt">Login</n-button>
@@ -25,49 +19,33 @@
     </div>
 </template>
 
-<script lang="ts">
-import {defineComponent, reactive} from 'vue'
-import {NConfigProvider, NInput, NDatePicker, NSpace} from 'naive-ui'
-import {encrypt,decrypt} from '@/utils/aes'
+<script lang="ts" setup>
+import { encrypt, decrypt } from '@/utils/aes'
 
 
-export default defineComponent({
-    components: {
-        NInput
-    },
-    setup() {
-        let formValue = reactive({
-            userName: '',
-            userPassword: '',
-            ciphertext: ''
-        })
-
-        function login() {
-            console.log('login')
-        }
-
-        function passwordEncrypt() {
-            formValue.ciphertext = encrypt(formValue.userPassword)
-            console.log(formValue.ciphertext, '密文')
-        }
-
-        function passwordDeciphering() {
-            let decrypts = decrypt(formValue.ciphertext)
-            console.log(decrypts,'decrypts')
-        }
-        onMounted(()=>{
-            console.log('onmounted')
-        })
-        return {
-            login,
-            formValue,
-            passwordEncrypt,
-            passwordDeciphering
-        }
-
-    },
-
+let formValue = reactive({
+    userName: '',
+    userPassword: '',
+    ciphertext: ''
 })
+
+function login() {
+    console.log('login')
+}
+
+function passwordEncrypt() {
+    formValue.ciphertext = encrypt(formValue.userPassword)
+    console.log(formValue.ciphertext, '密文')
+}
+
+function passwordDeciphering() {
+    let decrypts = decrypt(formValue.ciphertext)
+    console.log(decrypts, 'decrypts')
+}
+onMounted(() => {
+    console.log('onmounted')
+})
+
 </script>
 
 <style scoped>
@@ -109,6 +87,7 @@ export default defineComponent({
         -webkit-box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
         box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
     }
+
     100% {
         -webkit-box-shadow: 0 0 2px 0px rgba(0, 0, 0, 0.35);
         box-shadow: 0 0 2px 0px rgba(0, 0, 0, 0.35);
@@ -129,13 +108,13 @@ export default defineComponent({
 }
 
 .n-form-item .n-form-item-blank :focus-visible {
-    //outline: 1px #fe538d !important;
+    /* //outline: 1px #fe538d !important; */
 }
 
 .loginButton {
     width: 50%;
-    //color: #ffffff;
-    //background-color: #fe538d;
+    /* //color: #ffffff;
+    //background-color: #fe538d; */
     animation: shadow-drop-bottom 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
     border-radius: .3rem;
 }
@@ -145,6 +124,7 @@ export default defineComponent({
         -webkit-box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
         box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
     }
+
     100% {
         -webkit-box-shadow: 0 12px 20px -12px rgba(0, 0, 0, 0.35);
         box-shadow: 0 12px 20px -12px rgba(0, 0, 0, 0.35);
@@ -152,7 +132,7 @@ export default defineComponent({
 }
 
 .n-button:not(.n-button--disabled):active {
-    //border: 1px solid #fe538d !important;
-    //color: #fe538d;
+    /* //border: 1px solid #fe538d !important;
+    //color: #fe538d; */
 }
 </style>
