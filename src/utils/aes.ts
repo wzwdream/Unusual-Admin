46c9c7e49be34baafef6f004b783ca007736f0e4
wangzewei <wangzewei.cq@chinatelcom.cn>
2023-05-09 15:18:40 +0800

    加密算法添加异常处理

--- src/utils/aes.ts
+++ src/utils/aes.ts
@@ -10,23 +10,21 @@ const iv = enc.Utf8.parse('4FE46BE1C4485C95') // 偏移量
  * @param data 待加密的数据
  * @returns {string} 密文
  */
-export function encrypt(data: string | object): string {
-    if (typeof data === 'object') {
-        // 如果传入的data是json对象，先转义为json字符串
-        try {
-            data = JSON.stringify(data)
-        } catch (error) {
-            console.log('error:', error)
-        }
+export function encrypt(data: string): string {
+    if (typeof data !== 'string' || !data) return data
+    try {
+        // 统一将传入的字符串转成UTF8编码
+        const dataHex = enc.Utf8.parse(data) // 需要加密的数据
+        const encrypted = AES.encrypt(dataHex, key, {
+            iv,
+            mode: mode.CBC, // 加密模式
+            padding: pad.Pkcs7
+        })
+        return encrypted.ciphertext.toString() // 返回加密后的值
+    } catch(e) {
+        console.log('error', e)
+        return data
     }
-    // 统一将传入的字符串转成UTF8编码
-    const dataHex = enc.Utf8.parse(data as string) // 需要加密的数据
-    const encrypted = AES.encrypt(dataHex, key, {
-        iv,
-        mode: mode.CBC, // 加密模式
-        padding: pad.Pkcs7
-    })
-    return encrypted.ciphertext.toString() // 返回加密后的值
 }
 /**
  * 解密算法
@@ -35,13 +33,18 @@ export function encrypt(data: string | object): string {
  */
 export function decrypt(data: string): string {
     if (typeof data !== 'string' || !data) return data
-    const encryptedHexStr = enc.Hex.parse(data)
-    const srcs = enc.Base64.stringify(encryptedHexStr)
-    const decrypt = AES.decrypt(srcs, key, {
-        iv,
-        mode: mode.CBC,
-        padding: pad.Pkcs7
-    })
-    const decryptedStr = decrypt.toString(enc.Utf8)
-    return decryptedStr.toString()
+    try {
+        const encryptedHexStr = enc.Hex.parse(data)
+        const srcs = enc.Base64.stringify(encryptedHexStr)
+        const decrypt = AES.decrypt(srcs, key, {
+            iv,
+            mode: mode.CBC,
+            padding: pad.Pkcs7
+        })
+        const decryptedStr = decrypt.toString(enc.Utf8)
+        return decryptedStr.toString()
+    } catch(e) {
+        console.log('error', e)
+        return data
+    }
 }
