b45398c1133f982fde6ddba0593d768c17a6887b
yangyang <1758417159@qq.com>
2023-08-11 11:39:45 +0800

    feat:新增页面结构

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import menu from '@/components/menu.vue';
+
 </script>
 
 <template>
   <div class="home">
-    <router-view></router-view>
+    <div class="menu">
+      <Menu/>
+    </div>
+    <main class="main">
+      <router-view></router-view>
+    </main>
   </div>
-<!--  <router-view></router-view>-->
-<!--  <div>-->
-<!--    <a href="https://vitejs.dev" target="_blank">-->
-<!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
-<!--    </a>-->
-<!--    <a href="https://vuejs.org/" target="_blank">-->
-<!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
-<!--    </a>-->
-<!--  </div>-->
-<!--  <HelloWorld msg="Vite + Vue" />-->
+
+  <!--  <router-view></router-view>-->
+  <!--  <div>-->
+  <!--    <a href="https://vitejs.dev" target="_blank">-->
+  <!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
+  <!--    </a>-->
+  <!--    <a href="https://vuejs.org/" target="_blank">-->
+  <!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
+  <!--    </a>-->
+  <!--  </div>-->
+  <!--  <HelloWorld msg="Vite + Vue"/>-->
 </template>
 
 <style scoped>
@@ -25,15 +33,25 @@ import HelloWorld from './components/HelloWorld.vue'
   height: 100%;
   width: 100%;
   background-color: white;
+  display: flex;
+  .menu{
+    width: 210px;
+  }
+  .main{
+    flex-grow: 1;
+  }
 }
+
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
