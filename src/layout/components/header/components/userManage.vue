c5d43117a8069b752700a3cf52792d384aeced89
wangzewei <wangzewei.cq@chinatelecom.cn>
2023-09-12 14:29:40 +0800

    🐞 fix(header-用户): 修改布局

--- src/layout/components/header/components/userManage.vue
+++ src/layout/components/header/components/userManage.vue
@@ -1,33 +1,32 @@
 <template>
-  <n-popover trigger="hover" placement="bottom">
-    <template #trigger>
-      <n-space align="center" class="header-user">
-        <n-avatar round :size="32" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
-        <span>管理员</span>
-      </n-space>
-    </template>
-    <n-button-group vertical>
-      <n-button text>
-        <template #icon>
-          <Icon icon="material-symbols:manage-accounts-outline" />
-        </template>
-        {{ $t('header.personalSettings') }}
-      </n-button>
-      <n-button text>
-        <template #icon>
-          <Icon icon="ri:lock-line" />
-        </template>
-        {{ $t('header.modifyPassword') }}
-      </n-button>
-      <n-button text>
-        <template #icon>
-          <Icon icon="material-symbols:logout" />
-        </template>
-        {{ $t('header.logout') }}
-    </n-button>
-  </n-button-group>
-</n-popover>
+  <n-dropdown :options="options">
+    <n-space align="center" class="header-user">
+      <n-avatar round :size="32" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+      <span>管理员</span>
+    </n-space>
+  </n-dropdown>
 </template>
-<script setup lang="ts" name="UserManage">
 
+<script setup lang="ts" name="UserManage">
+import { renderIcon } from '@/utils/help';
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+
+const options = computed(() => [
+  {
+    label: t('header.personalSettings'),
+    key: 'profile',
+    icon: renderIcon('material-symbols:manage-accounts-outline')
+  },
+  {
+    label: t('header.modifyPassword'),
+    key: 'editProfile',
+    icon: renderIcon('ri:lock-line')
+  },
+  {
+    label: t('header.logout'),
+    key: 'logout',
+    icon: renderIcon('material-symbols:logout')
+  }
+])
 </script>
