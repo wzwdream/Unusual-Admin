da0de9bb7994cda70891fe3c19db1145127b1bba
yangyang <1758417159@qq.com>
2023-10-16 15:21:48 +0800

    feat:email页面阶段性完成

--- src/views/email.vue
+++ src/views/email.vue
@@ -1,36 +1,40 @@
 <script lang="ts" setup>
-import {FormInst, FormRules} from 'naive-ui'
+import {FormInst, FormRules, FormItemRule, useMessage} from 'naive-ui'
 import {getTestDataArray, sendEmail} from '@/api/user/mail';
 
 // import GraphemeSplitter from 'grapheme-splitter'
 interface ModelType {
-  addressee: string | null
+  email: string | null
   subject: string | null
   text: string | null
 }
 
 const formRef = ref<FormInst | null>(null)
 // const rPasswordFormItemRef = ref<FormItemInst | null>(null)
-// const message = useMessage()
+const message = useMessage()
 // const splitter = new GraphemeSplitter()
 const emailData = ref<ModelType>({
-  addressee: '1758417159@qq.com',
+  email: '1758417159@qq.com',
   subject: '测试',
-  text:  '测试',
+  text: '测试',
 })
 // function countGraphemes(value: string) {
 //     splitter.countGraphemes(value)
 // }
 const rules: FormRules = {
-  addressee: [
+  email: [
     {
       required: true,
       trigger: ['input', 'blur'],
+      validator(rule: FormItemRule, value: string) {
+        return /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(.[a-zA-Z0-9_-]+)+$/.test(value)
+      },
     },
   ],
   subject: [
     {
       required: true,
+      trigger: ['input', 'blur'],
       message: '请输入密码',
     },
   ],
@@ -45,14 +49,17 @@ const rules: FormRules = {
 getTestDataArray().then(res => {
   console.table(res.data)
 })
-function sendEmailHandle (){
-  formRef.value?.validate((errors)=>{
-    if (!errors){
-      sendEmail({...emailData.value}).then(res=>{
-        console.log(res)
+
+function sendEmailHandle() {
+  formRef.value?.validate((errors) => {
+    if (!errors) {
+      sendEmail({...emailData.value}).then(res => {
+        if (res.code === 200){
+          message.success('success')
+        }
       })
-    }else {
-      console.error(22)
+    } else {
+      console.error('Verification failure')
     }
   })
 
@@ -63,16 +70,15 @@ function sendEmailHandle (){
   <div class="">
     <div class='w-1/3 m-auto p-10 '>
       <n-form ref="formRef" :model="emailData" :rules="rules">
-        <n-form-item path="age" label="收件人">
-          <n-input v-model:value="emailData.addressee" @keydown.enter.prevent maxlength="30" clearable show-count/>
+        <n-form-item path="email" label="收件人">
+          <n-input v-model:value="emailData.email" maxlength="30" clearable show-count/>
         </n-form-item>
-        <n-form-item path="password" label="主题">
-          <n-input v-model:value="emailData.subject" @keydown.enter.prevent maxlength="30" clearable show-count/>
+        <n-form-item path="subject" label="主题">
+          <n-input v-model:value="emailData.subject" maxlength="30" clearable show-count/>
         </n-form-item>
-        <n-form-item label="内容">
+        <n-form-item path="text" label="内容">
           <n-input
               v-model:value="emailData.text"
-              @keydown.enter.prevent
               type="textarea"
               placeholder="请输入"
               maxlength="1000"
@@ -83,7 +89,9 @@ function sendEmailHandle (){
         <n-row :gutter="[0, 24]">
           <n-col :span="24">
             <div class="flex justify-end">
-              <n-button :disabled="emailData.addressee === null" round type="primary" @click="sendEmailHandle()"> 发送</n-button>
+              <n-button :disabled="emailData.email === null" round type="primary" @keydown.enter.prevent
+                        @click="sendEmailHandle()"> 发送
+              </n-button>
             </div>
           </n-col>
         </n-row>
