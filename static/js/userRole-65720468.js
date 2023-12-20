4a5a04736a35c2e2dfffb98fca3c705325f43fb0
wzwdream <dream_run@126.com>
2023-12-20 02:17:59 +0000

    Deploying to pages from @ wzwdream/Unusual-Admin@b11b9c1c71e1aa83320d8670199bf3dcb11be653 🚀

---
+++ static/js/userRole-65720468.js
@@ -0,0 +1,1 @@
+import{h as e}from"./index-df40bf96.js";const o=()=>e.get("/userRole"),s=o=>e.put("/updateRole",o),t=o=>e.delete("/deleteRole",o),a=o=>e.post("/addRole",o);export{a,t as d,o as g,s as u};
